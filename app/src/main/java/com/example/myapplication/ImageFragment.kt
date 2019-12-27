ec025f6b8d9a9349c628350c729ea903d48043c1
JaneYi <janeyi@naver.com>
2019-12-27 22:49:57 +0900

    add 2 buttons (linear layout version)

--- app/src/main/java/com/example/myapplication/ImageFragment.kt
+++ app/src/main/java/com/example/myapplication/ImageFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.content.ContextCompat.getDrawable
 import androidx.recyclerview.widget.*
 
@@ -15,6 +16,7 @@ class ImageFragment : Fragment() {
     lateinit var ImageRecyclerView : RecyclerView
 
     var isfirst : Boolean = true
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,8 +31,12 @@ class ImageFragment : Fragment() {
 
         var rootView = inflater.inflate(R.layout.fragment_image, container, false)
 
+        var insertButton : Button = rootView.findViewById(R.id.plusbutton)
+        var deleteButton : Button = rootView.findViewById(R.id.minusbutton)
+
         ImageRecyclerView = rootView.findViewById(R.id.recyclerView!!)as RecyclerView
         ImageRecyclerView.adapter = ImageRecyclerAdapter(image_list)
+
 
         return rootView
     }
@@ -41,6 +47,8 @@ class ImageFragment : Fragment() {
         image_list.add(ImageItem(getResources().getDrawable(R.drawable.image03)!!, getString(R.string.title03)))
         image_list.add(ImageItem(getResources().getDrawable(R.drawable.image04)!!, getString(R.string.title04)))
         image_list.add(ImageItem(getResources().getDrawable(R.drawable.image05)!!, getString(R.string.title05)))
+        image_list.add(ImageItem(getResources().getDrawable(R.drawable.image06)!!, getString(R.string.title06)))
+        image_list.add(ImageItem(getResources().getDrawable(R.drawable.image07)!!, getString(R.string.title07)))
     }
 
 
