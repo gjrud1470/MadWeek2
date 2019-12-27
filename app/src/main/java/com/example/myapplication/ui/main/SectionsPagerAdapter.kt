fff2ddad87b98b229d1314611ae10dfb8b9df3f5
gjrud1470 <gjrud1470@kaist.ac.kr>
2019-12-27 14:19:01 +0900

    Added blank fragments

--- app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.myapplication.ContactsFragment
+import com.example.myapplication.HomeFragment
+import com.example.myapplication.ImageFragment
 import com.example.myapplication.R
 
 private val TAB_TITLES = arrayOf(
@@ -23,7 +26,13 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return when (position) {
+            0 -> {HomeFragment()}
+            1 -> {ContactsFragment()}
+            2 -> {ImageFragment()}
+            else -> PlaceholderFragment.newInstance(position + 1)
+        }
+        //return PlaceholderFragment.newInstance(position + 1)
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
