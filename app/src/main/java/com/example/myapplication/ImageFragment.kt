dfb103647b9f31e2eb8e5f2b7b27581b7e4308ad
JaneYi <janeyi@naver.com>
2019-12-27 18:03:35 +0900

    show 3 images

--- app/src/main/java/com/example/myapplication/ImageFragment.kt
+++ app/src/main/java/com/example/myapplication/ImageFragment.kt
@@ -6,18 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.recyclerview.widget.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class ImageFragment : Fragment() {
+
+    val image_list : ArrayList<ImageItem> = ArrayList()
+    lateinit var ImageRecyclerView : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false)
+
+        image_list.add(ImageItem(getResources().getDrawable(R.drawable.image01)!!, getString(R.string.title01)))
+        image_list.add(ImageItem(getResources().getDrawable(R.drawable.image02)!!, getString(R.string.title02)))
+        image_list.add(ImageItem(getResources().getDrawable(R.drawable.image03)!!, getString(R.string.title03)))
+
+        var rootView = inflater.inflate(R.layout.fragment_image, container, false)
+
+        ImageRecyclerView = rootView.findViewById(R.id.recyclerView!!)as RecyclerView
+        ImageRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        ImageRecyclerView.adapter = ImageRecyclerAdapter(image_list)
+
+        return rootView
+
     }
 
 
---
+++ app/src/main/java/com/example/myapplication/ImageItem.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+import android.graphics.drawable.Drawable
+
+class ImageItem(val image: Drawable, val title: String) {
+}
