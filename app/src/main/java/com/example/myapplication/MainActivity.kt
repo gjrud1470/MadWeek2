ee04569d02d51d2ffe9c0f749dfc13849bf82d1f
JaneYi <janeyi@naver.com>
2019-12-31 22:34:24 +0900

    view pager non swipe

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.Manifest
 import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
@@ -20,6 +21,40 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.os.Build
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.core.view.MotionEventCompat
+
+
+class nonSwipeViewPager(context: Context, attributeSet: AttributeSet): ViewPager(context, attributeSet) {
+    var enable: Boolean = false
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        if(enable){
+            return super.onInterceptTouchEvent(ev)
+        }
+        else{
+            if(MotionEventCompat.getActionMasked(ev)==MotionEvent.ACTION_MOVE){
+                // ㄴㄴ
+            }
+            else{
+                if(super.onInterceptTouchEvent(ev)){
+                    super.onTouchEvent(ev)
+                }
+            }
+            return false
+        }
+    }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean{
+        if(enable){
+            return super.onTouchEvent(ev)
+        }
+        else{
+            return MotionEventCompat.getActionMasked(ev) != MotionEvent.ACTION_MOVE && super.onTouchEvent(ev)
+        }
+    }
+}
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,7 +67,7 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPager: nonSwipeViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = this.findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
